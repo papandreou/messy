37718526708fed7d3b1bed52d3ae5329f349bfa6
Andreas Lind <andreas@one.com>
2014-08-16 21:09:47 +0200

    Added test.

--- test/Headers.js
+++ test/Headers.js
@@ -3,6 +3,12 @@ var expect = require('unexpected'),
     Headers = require('../lib/Headers');
 
 describe('Headers', function () {
+    it('should accept a string', function () {
+        var headers = new Headers('Subject: hey, dude!');
+        expect(headers.get('subject'), 'to equal', 'hey, dude!');
+        expect(headers.toString(), 'to equal', 'Subject: hey, dude!\r\n');
+    });
+
     it('should fold the lines when serializing', function () {
         var headers = new Headers({subject: 'hey there, dude!'});
         expect(headers.toString(10), 'to equal', 'Subject:\r\n hey\r\n there,\r\n dude!\r\n');
