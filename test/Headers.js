ea5196da5b5750da5b939564ca5a4ea1a49abc22
Andreas Lind <andreas@one.com>
2014-11-28 16:59:47 +0100

    Fixed folding of header lines.

--- test/Headers.js
+++ test/Headers.js
@@ -18,7 +18,7 @@ describe('Headers', function () {
 
     it('should fold the lines when serializing', function () {
         var headers = new Headers({subject: 'hey there, dude!'});
-        expect(headers.toString(10), 'to equal', 'Subject:\r\n hey\r\n there,\r\n dude!\r\n');
+        expect(headers.toString(10), 'to equal', 'Subject: hey\r\n there,\r\n dude!\r\n');
     });
 
     it('should accept an array header value when instantiating via an Object', function () {
