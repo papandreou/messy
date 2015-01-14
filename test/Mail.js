e26be8d3b69850c50aa8b4cc0209c4aca6a27f6a
Andreas Lind <andreas@one.com>
2015-01-14 14:18:50 +0100

    Message.fileName getter/setter: Fall back to/also set the 'name' property of Content-Type when in Mail/HeadersWithRfc2047 mode.

--- test/Mail.js
+++ test/Mail.js
@@ -13,4 +13,40 @@ describe('Mail', function () {
         mail.headers.set('subject', '¡Hola, señor!');
         expect(mail.toString(), 'to equal', 'Subject: =?iso-8859-1?Q?=A1Hola=2C?= =?iso-8859-1?Q?_se=F1or!?=\r\n\r\nbar');
     });
+
+    describe('#fileName', function () {
+        describe('when invoked as a getter', function () {
+            it('should fall back to the name property of the Content-Type header when the Content-Disposition header has no filename parameter', function () {
+                var mail = new Mail(
+                    'Content-Transfer-Encoding: base64\r\n' +
+                    'Content-Disposition: attachment\r\n' +
+                    'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
+                );
+                expect(mail.fileName, 'to equal', 'æøå.png');
+            });
+
+            it('should fall back to the name property of the Content-Type header when there is no Content-Disposition header', function () {
+                var mail = new Mail(
+                    'Content-Transfer-Encoding: base64\r\n' +
+                    'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="'
+                );
+                expect(mail.fileName, 'to equal', 'æøå.png');
+            });
+        });
+
+        describe('when invoked as a setter', function () {
+            it('should update the name property of the Content-Type header if available', function () {
+                var mail = new Mail({headers: {'Content-Type': 'image/png'}});
+                mail.fileName = 'æøå.png';
+                expect(
+                    mail.toString(),
+                    'to equal',
+                    // TODO: Would be better to emit 'Content-Type: image/png; name="=?iso-8859-1?Q?=E6=F8=E5.png?="\r\n' +
+                    'Content-Type: image/png; =?iso-8859-1?Q?name==E6=F8=E5?=.png\r\n' +
+                    'Content-Disposition: attachment;\r\n' +
+                    " filename*=iso-8859-1''%E6%F8%E5%2E%70%6E%67\r\n"
+                );
+            });
+        });
+    });
 });
