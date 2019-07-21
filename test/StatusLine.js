2a82da4c25a5940cc437e4b906fcbaa5690e1bd9
Andreas Lind <andreaslindpetersen@gmail.com>
2019-07-21 17:17:00 +0200

    prettier --write '**/*.js'

--- test/StatusLine.js
+++ test/StatusLine.js
@@ -1,22 +1,26 @@
 /* global describe, it */
-var expect = require('unexpected');
-    var StatusLine = require('../lib/StatusLine');
+var expect = require("unexpected");
+var StatusLine = require("../lib/StatusLine");
 
-describe('StatusLine', function () {
-    describe('#toJSON', function () {
-        it('should return non-computed properties', function () {
-            expect(new StatusLine('HTTP/1.1 200 OK').toJSON(), 'to equal', {
-                statusCode: 200,
-                statusMessage: 'OK',
-                protocolName: 'HTTP',
-                protocolVersion: '1.1'
-            });
-        });
-
-        // Makes it possible to use statusLine.toJSON() as the RHS of a 'to satisfy' assertion in Unexpected
-        // where undefined means that the property must not be present:
-        it('should not include the keys that have undefined values', function () {
-            expect(new StatusLine('HTTP/1.1').toJSON(), 'not to have key', 'statusCode');
-        });
+describe("StatusLine", function() {
+  describe("#toJSON", function() {
+    it("should return non-computed properties", function() {
+      expect(new StatusLine("HTTP/1.1 200 OK").toJSON(), "to equal", {
+        statusCode: 200,
+        statusMessage: "OK",
+        protocolName: "HTTP",
+        protocolVersion: "1.1"
+      });
     });
+
+    // Makes it possible to use statusLine.toJSON() as the RHS of a 'to satisfy' assertion in Unexpected
+    // where undefined means that the property must not be present:
+    it("should not include the keys that have undefined values", function() {
+      expect(
+        new StatusLine("HTTP/1.1").toJSON(),
+        "not to have key",
+        "statusCode"
+      );
+    });
+  });
 });
